aac411d8c23b1582f21d68e576c27111e912d463
Gabriel Lima <gabrielvitor180701@gmail.com>
2026-09-16 17:51:56 -0300

    feat: updating schema

--- app/schemas.py
+++ app/schemas.py
@@ -20,3 +20,28 @@ class Customer(BaseModel):
     cons_conf_idx: float
     euribor3m: float
     nr_employed: float
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "age": 35,
+                "job": "admin.",
+                "marital": "married",
+                "education": "university.degree",
+                "default": "no",
+                "housing": "yes",
+                "loan": "no",
+                "month": "may",
+                "day_of_week": "mon",
+                "campaign": 1,
+                "pdays": 999,
+                "previous": 0,
+                "poutcome": "nonexistent",
+                "emp_var_rate": 1.1,
+                "cons_price_idx": 93.994,
+                "cons_conf_idx": -36.4,
+                "euribor3m": 4.857,
+                "nr_employed": 5191.0
+            }
+        }
+    }
